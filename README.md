b1d92c48b196e49d1340d9f1e1f139a6fbd17f68
robz <r@r.com>
2022-12-01 11:51:02 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CalcPackDos
+
+A description of this package.
---
+++ Sources/CalcPackDos/CalcPackDos.swift
@@ -0,0 +1,17 @@
+public struct CalcPackDos {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
+
+
+public class CalculatorDos {
+    public init() {}
+    public func multiply(_ valueX: Int, with ValueY: Int) -> Int {
+        valueX * ValueY
+    }
+    public func add(_ valueX: Int, ValueY: Int) -> Int {
+        valueX + ValueY
+    }
+}
